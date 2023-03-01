4c2a38716247b97c801e7ad1715d5a0fbd77189c
tatsumaki <ersangemici2011@gmail.com>
2023-03-01 21:21:13 +0300

    Project Complated

--- main.py
+++ main.py
@@ -47,9 +47,7 @@ class SadePizza(Pizza):
 
     def get_cost(self):
         return self.cost
-
-
-class Decoator(Pizza):
+class Decorator(Pizza):
     def __init__(self, component):
         self.component = component
         
@@ -61,48 +59,46 @@ class Decoator(Pizza):
         return self.component.get_description() + ' ' + Pizza.get_description(self)
 
 
-class ZeytinSos(Decoator):
+class ZeytinSos(Decorator):
     def __init__(self, component):
-        Decoator.__init__(self, component)
+        Decorator.__init__(self, component)
         self.description = "Zeytin"
         self.cost = 1.0
 
 
-class MantarSos(Decoator):
+class MantarSos(Decorator):
     def __init__(self, component):
-        Decoator.__init__(self, component)
+        Decorator.__init__(self, component)
         self.description = "Mantar"
         self.cost = 1.5
 
 
-class KeciPeyniriSos(Decoator):
+class KeciPeyniriSos(Decorator):
     def __init__(self, component):
-        Decoator.__init__(self, component)
+        Decorator.__init__(self, component)
         self.description = "Keci Peyniri"
         self.cost = 2.0
 
 
-class EtSos(Decoator):
+class EtSos(Decorator):
     def __init__(self, component):
-        Decoator.__init__(self, component)
+        Decorator.__init__(self, component)
         self.description = "Et"
         self.cost = 2.5
 
 
-class SoganSos(Decoator):
+class SoganSos(Decorator):
     def __init__(self, component):
-        Decoator.__init__(self, component)
+        Decorator.__init__(self, component)
         self.description = "Sogan"
         self.cost = 1.0
 
 
-class MisirSos(Decoator):
+class MisirSos(Decorator):
     def __init__(self, component):
-        Decoator.__init__(self, component)
+        Decorator.__init__(self, component)
         self.description = "Misir"
         self.cost = 1.0
-
-
 def print_menu():
     with open('menu.txt', 'r') as file:
         data = file.read()
@@ -126,47 +122,67 @@ def main():
             print("Lütfen geçerli bir sayı giriniz.")
 
     # Sos seçimi
+    sos_list=[]
     while True:
-        sos_list=[]
         try:
-            sos_choice = int(input("Lütfen bir sos seçiniz (11-16): "))
-            if sos_choice < 11 or sos_choice > 16:
-                print("Lütfen geçerli bir seçim yapınız.")
-
-            else:
+            sos_choice = int(input("Lütfen bir sos seçiniz (11-16): Siparişi Onaylayıp Devam Etmek için (0)"))
+            if sos_choice==0:
                 break
+            elif sos_choice < 11 or sos_choice > 16:
+                print("Lütfen geçerli bir seçim yapınız.")
+            else:
+                sos_list.append(sos_choice)
+                continue
         except ValueError:
             print("Lütfen geçerli bir sayı giriniz.")
-        sos_list.append(sos_choice)
-
+    print(sos_list)
+    while sos_choice==0:
     # Seçilen pizza ve sosların fiyatlarını hesaplama
-    if pizza_choice == 1:
-        pizza = KlasikPizza()
-    elif pizza_choice == 2:
-        pizza = MargaritaPizza()
-    elif pizza_choice == 3:
-        pizza = TurkPizza()
-    else:
-        pizza = SadePizza()
+        if pizza_choice == 1:
+            pizza = KlasikPizza()
+            break
+        elif pizza_choice == 2:
+            pizza = MargaritaPizza()
+            break
+        elif pizza_choice == 3:
+            pizza = TurkPizza()
+            break
+        else:
+            pizza = SadePizza()
+            break
 
-    if sos_choice == 11:
-        sos = ZeytinSos(pizza)
-    elif sos_choice == 12:
-        sos = MantarSos(pizza)
-    elif sos_choice == 13:
-        sos = KeciPeyniriSos(pizza)
-    elif sos_choice == 14:
-        sos = EtSos(pizza)
-    elif sos_choice == 15:
-        sos = SoganSos(pizza)
-    else:
-        sos = MisirSos(pizza)
+    pizza_base=pizza.get_cost()
+    sos_cost=0
+    sos_description=""
+    for element in sos_list:
+            if element == 11:
+                sos_cost += ZeytinSos(pizza).cost
+                sos_description+=" "+ZeytinSos(pizza).description
+            elif element == 12:
+                sos_cost += MantarSos(pizza).cost
+                sos_description+=" "+MantarSos(pizza).description
+                
+            elif element == 13:
+                sos_cost += KeciPeyniriSos(pizza).cost
+                sos_description+=" "+KeciPeyniriSos(pizza).description
 
-    total_cost = sos.get_cost()
-    description = sos.get_description()
+            elif element == 14:
+                sos_cost += EtSos(pizza).cost
+                sos_description+=" "+EtSos(pizza).description
+
+            elif element == 15:
+                sos_cost += SoganSos(pizza).cost
+                sos_description+=" "+SoganSos(pizza).description
+
+            else:
+                sos_description+=" "+MisirSos(pizza).description
+                sos_cost +=MisirSos(pizza).cost
+
+    total_cost = pizza_base+sos_cost
+    description = pizza.get_description()+sos_description
 
     print("Seçilen pizza: {}".format(pizza.__class__.__name__))
-    print("Seçilen sos: {}".format(sos.__class__.__name__))
+    print("Seçilen soslar: {}".format(sos_description))
     print("Toplam fiyat: {} TL".format(total_cost))
     print("Açıklama: {}".format(description))
     if input("Siparişi Onaylıyor Musunuz? Evet/Hayır").lower()=="evet":
@@ -185,7 +201,6 @@ def main():
         with open('Orders_Database.csv', mode='a', newline='') as file:
             filednames=['Username','TC','Card Number','Order Description','Order Time','Card Pin']
             writer=csv.DictWriter(file,fieldnames=filednames)
-            writer.writeheader()
             writer.writerow(order)
         print("Sipariş başarılı!")
     else:
