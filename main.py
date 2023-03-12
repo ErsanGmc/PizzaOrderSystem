cec97a55da8f6135a8c2e860bc5407d0b39c16d1
tatsumaki <ersangemici2011@gmail.com>
2023-03-12 13:37:19 +0300

    you can choose just one sauce

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import datetime
 
 class Pizza:
     def __init__(self):
-        self.cost=0
-        self.description = "Unknown Pizza"
+        self.cost=0.0
+        self.description = ""
 
     def get_description(self):
         return self.description
@@ -15,23 +15,14 @@ class Pizza:
 
 class KlasikPizza(Pizza):
     def __init__(self):
-        self.description = "Classic Pizza"
+        self.description = "Klasik Pizza"
         self.cost = 10.0
-    def get_description(self):
-        return super().get_description()
-    def get_cost(self):
-        return super().get_cost()
 
 
 class MargaritaPizza(Pizza):
     def __init__(self):
-        self.description = "Margherita Pizza"
+        self.description = "Margarita Pizza"
         self.cost = 8.0
-
-    def get_cost(self):
-        return super().get_cost()
-    def get_description(self):
-        return super().get_description()
 
 
 class TurkPizza(Pizza):
@@ -39,10 +30,6 @@ class TurkPizza(Pizza):
         self.description = "Turkish Pizza"
         self.cost = 12.0
 
-    def get_cost(self):
-        return super().get_cost()
-    def get_description(self):
-        return super().get_description()
 
 
 class SadePizza(Pizza):
@@ -50,144 +37,127 @@ class SadePizza(Pizza):
         self.description = "Dominos Pizza"
         self.cost = 15.0
 
-    def get_cost(self):
-        return super().get_cost()
-    def get_description(self):
-        return super().get_description()
 class Decorator(Pizza):
-    def __init__(self, component):
-        self.component = component
+    def __init__(self, sauces):
+        self.sauces = sauces
         
     def get_cost(self):
-        return self.component.get_cost() + Pizza.get_cost(self)
+        return self.sauces.get_cost() + Pizza.get_cost(self)
 
     def get_description(self):
         
-        return self.component.get_description() + ' ' + Pizza.get_description(self)
+        return self.sauces.get_description() + ':' + Pizza.get_description(self)
 
 
 class ZeytinSos(Decorator):
-    def __init__(self, component):
-        Decorator.__init__(self, component)
+    def __init__(self, sauces):
+        self.sauces=sauces
         self.description = "Zeytin"
         self.cost = 1.0
 
 
 class MantarSos(Decorator):
-    def __init__(self, component):
-        Decorator.__init__(self, component)
+    def __init__(self, sauces):
+        self.sauces=sauces
         self.description = "Mantar"
         self.cost = 1.5
 
 
 class KeciPeyniriSos(Decorator):
-    def __init__(self, component):
-        Decorator.__init__(self, component)
+    def __init__(self, sauces):
+        self.sauces=sauces
         self.description = "Keci Peyniri"
         self.cost = 2.0
 
 
 class EtSos(Decorator):
-    def __init__(self, component):
-        Decorator.__init__(self, component)
+    def __init__(self, sauces):
+        self.sauces=sauces
         self.description = "Et"
         self.cost = 2.5
 
 
 class SoganSos(Decorator):
-    def __init__(self, component):
-        Decorator.__init__(self, component)
+    def __init__(self, sauces):
+        self.sauces=sauces
         self.description = "Sogan"
         self.cost = 1.0
 
 
 class MisirSos(Decorator):
-    def __init__(self, component):
-        Decorator.__init__(self, component)
+    def __init__(self, sauces):
+        self.sauces=sauces
         self.description = "Misir"
         self.cost = 1.0
 def print_menu():
     with open('menu.txt', 'r') as file:
         data = file.read()
         print(data)
-
+def menu_write():
+    with open('menu.txt',mode='w') as file:
+        menu="* Please Choose Pizza Base: \n1: Klasik\n2: Margarita\n3: TurkPizza\n4: Sade Pizza\n* ve sececeginiz sos:\n11: Zeytin\n12: Mantarlar\n13: Keci Peyniri\n14: Et\n15: Sogan\n16: Misir\n\n* Tesekkur ederiz!"
+        file.write(menu)
 
 def main():
+    menu_write()
     print_menu()
 
     # Pizza seçimi
-    while True:
+    pizza=""
+    while pizza=="":
         try:
-            pizza_choice = int(input("Lütfen bir pizza seçiniz (1-4): "))
-            if pizza_choice < 1 or pizza_choice > 4:
+            choosePizza = int(input("Lütfen bir pizza seçiniz (1-4): "))
+            if choosePizza < 1 or choosePizza > 4:
                 print("Lütfen geçerli bir seçim yapınız.")
             else:
-                break
+                if choosePizza == 1:
+                    pizza = KlasikPizza()
+                    
+                elif choosePizza == 2:
+                    pizza = MargaritaPizza()
+                    
+                elif choosePizza == 3:
+                    pizza = TurkPizza()
+                    
+                else:
+                    pizza = SadePizza()
+                    
+                
         except ValueError:
             print("Lütfen geçerli bir sayı giriniz.")
+    
 
     # Sos seçimi
-    sos_list=[]
-    while True:
+    sauce=""
+    while sauce=="":
         try:
             sos_choice = int(input("Lütfen bir sos seçiniz (11-16): Siparişi Onaylayıp Devam Etmek için (0)"))
-            if sos_choice==0:
-                break
-            elif sos_choice < 11 or sos_choice > 16:
+            if sos_choice < 11 or sos_choice > 16:
                 print("Lütfen geçerli bir seçim yapınız.")
             else:
-                sos_list.append(sos_choice)
-                continue
+                if sos_choice == 11:
+                    sauce = ZeytinSos(pizza)                   
+                elif sos_choice == 12:
+                    sauce = MantarSos(pizza)                   
+                elif sos_choice == 13:
+                    sauce = KeciPeyniriSos(pizza)               
+                elif sos_choice == 14:
+                    sauce = EtSos(pizza)                  
+                elif sos_choice == 15:
+                    sauce = SoganSos(pizza)      
+                elif sos_choice == 16:
+                    sauce = MisirSos(pizza)
+                           
         except ValueError:
             print("Lütfen geçerli bir sayı giriniz.")
-    print(sos_list)
-    while sos_choice==0:
-    # Seçilen pizza ve sosların fiyatlarını hesaplama
-        if pizza_choice == 1:
-            pizza = KlasikPizza()
-            break
-        elif pizza_choice == 2:
-            pizza = MargaritaPizza()
-            break
-        elif pizza_choice == 3:
-            pizza = TurkPizza()
-            break
-        else:
-            pizza = SadePizza()
-            break
+    
 
-    pizza_base=pizza.get_cost()
-    sos_cost=0
-    sos_description=""
-    for element in sos_list:
-            if element == 11:
-                sos_cost += ZeytinSos(pizza).cost
-                sos_description+=" "+ZeytinSos(pizza).description
-            elif element == 12:
-                sos_cost += MantarSos(pizza).cost
-                sos_description+=" "+MantarSos(pizza).description
-                
-            elif element == 13:
-                sos_cost += KeciPeyniriSos(pizza).cost
-                sos_description+=" "+KeciPeyniriSos(pizza).description
 
-            elif element == 14:
-                sos_cost += EtSos(pizza).cost
-                sos_description+=" "+EtSos(pizza).description
-
-            elif element == 15:
-                sos_cost += SoganSos(pizza).cost
-                sos_description+=" "+SoganSos(pizza).description
-
-            else:
-                sos_description+=" "+MisirSos(pizza).description
-                sos_cost +=MisirSos(pizza).cost
-
-    total_cost = pizza_base+sos_cost
-    description = pizza.get_description()+sos_description
+    total_cost = sauce.get_cost()
+    description = sauce.get_description()
 
     print("Seçilen pizza: {}".format(pizza.__class__.__name__))
-    print("Seçilen soslar: {}".format(sos_description))
+    print("Seçilen soslar: {}".format(sauce.__class__.__name__))
     print("Toplam fiyat: {} $".format(total_cost))
     print("Açıklama: {}".format(description))
     if input("Siparişi Onaylıyor Musunuz? Evet/Hayır\n").lower()=="evet":
@@ -200,7 +170,7 @@ def main():
             "TC":tc_no,
             "Card Number":card_number,
             "Order Description":description,
-            "Order Time":datetime.datetime.now(),
+            "Order Time":datetime.datetime.now().strftime("%d-%m-%Y %H:%M"),
             "Card Pin":card_pin
         }
         with open('Orders_Database.csv', mode='a', newline='') as file:
